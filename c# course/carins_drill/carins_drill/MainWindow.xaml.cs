2df3274c457715aae6a1e472b56885fdb1284c32
leblancsm91 <leblancsm91@gmail.com>
2019-07-19 15:53:15 -0700

    [added new drill]

---
+++ c# course/carins_drill/carins_drill/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{ 
+    static void Main()
+        {
+        Console.WriteLine("What is your age?");
+        int age = Console.ReadLine(if > 15 = true, else = false);
+
+        Console.WriteLine("Have you ever had a DUI?");
+        bool dui = Console.ReadLine(if no = true, else = false);
+
+        Console.WriteLine("How many sppeeding tickets do you have?");
+        int tickets = Console.ReadLine(if < 3 = true, else = false);
+
+        Console.WriteLine("you are " + answer + "qualified for car insurance with this fake insurance company.");
+        bool answer = Console.ReadLine(if true = yes, else if false = no);
+    }
+
+}
