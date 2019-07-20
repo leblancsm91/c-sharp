ba1e10f9b8c00aa5bbef690769578decf76bf177
leblancsm91 <leblancsm91@gmail.com>
2019-07-20 12:38:18 -0700

    [added drills proj]

---
+++ c# course/bool_comparisondrillDOWHILE/bool_comparisondrillDOWHILE/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boolean_comparisondrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type a color");
+            string color = Console.ReadLine();
+            bool chosenColor = false;
+
+            do
+            {
+                switch (color)
+                {
+                    case yellow:
+                        Console.WriteLine("yellow is not hte color. please pick again");
+                        break;
+
+                    case green:
+                        Console.WriteLine("green is not hte color. please choose again.");
+                        break;
+
+                    case blue:
+                        Console.WriteLine("yes! this is the color!");
+                        break;
+                }
+            }
+            while (!chosenColor);
+            Console.ReadLine();
+        }
+    }
+
+}
+}
