4f4bf7280347a083f10be271c2bb54fdf5bf8a8f
leblancsm91 <leblancsm91@gmail.com>
2019-07-21 14:37:02 -0700

    [added new drill]

---
+++ c# course/string_drill/string_drill/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            string color = "blue";
+            string rainbow = "the rainbow consists of; \n red\n orange\n yellow\n green\n blue\n purple\n and most importantly - skittles !!!!";
+            string question = "What is the second ot last color in a rainbow?";
+
+            color = color.ToUpper();
+
+            StringBuilder skittles = new StringBuilder();
+            skittles.Append("skittles are in the rainbow.\n taste the rainbow");
+
+            Console.WriteLine(rainbow);
+            Console.WriteLine(question);
+            Console.ReadLine();
+        }
+    }
+}
