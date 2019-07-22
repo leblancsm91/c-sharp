0791f2f816f3e805d4b009c7c138f9aee482fdd5
leblancsm91 <leblancsm91@gmail.com>
2019-07-22 08:14:59 -0700

    [added new drill]

---
+++ c# course/array-string_Drill/array-string_Drill/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main();
+
+    string[] colorArray = new string[] { red, orange, yellow, green, blue, purple };
+
+
+    Console.WriteLine("please pick a box from this array" + colorArray[] + );
+    Console.ReadLine(if 0, 1, 2, 3, 4, 5 = colorArray[0], colorArray[1], colorArray[2], colorArray[3], colorArray[4], colorArray[5], else = "this is not a real box of array");
+
+    in[] numArray = new int[6];
+    numArray[0] = 10;
+    numArray[1] = 100;
+    numArray[2] = 20;
+    numArray[3] = 200;
+    numArray[4] = 30;
+    numArray[5] = 300;
+
+    Console.WriteLine("please choose a box from this array" + numArray[] + );
+    Console.ReadLine(if 0, 1, 2, 3, 4, 5 = numArray[0], numArray[1], numArray[2], numArray[3], numArray[4], numArray[5], else = "that is not an array box");
+    
+        //list
+    List<string> intList = new List<string>();
+    stringList.Add(red);
+    stringList.Add(orange);
+    stringList.Add(yellow);
+    stringList.Add(green);
+    stringList.Add(blue);
+    stringList.Add(purple);
+
+    Console.WriteLine(intList[0]); //<-always assume have array!
+    Console.ReadLine();
+
+}
