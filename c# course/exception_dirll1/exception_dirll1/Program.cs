fd7b5bd67a9f7f81f1b89e82db7fd515e4b91644
leblancsm91 <leblancsm91@gmail.com>
2019-07-24 11:34:53 -0700

    [added drill]

---
+++ c# course/exception_dirll1/exception_dirll1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> intList = new List<int>();
+        intList.Add(50);
+        intList.Add(30);
+        intList.Add(10);
+
+        Console.ReadLine();
+
+        try
+        {
+            Console.WriteLine("pick a number");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int number in intList)
+            {
+                int answer1 = number / numberOne;
+                Console.WriteLine(answer1);
+            }
+        }
+
+        catch (FormatException ex)
+        {
+            Console.WriteLine("type whole number only");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("don't divide by zero");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.WriteLine("the program is continuinig");
+        Console.ReadLine();
+    }
+}
+
