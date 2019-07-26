0f4044df761e464e97107130df111e4736bfdfba
leblancsm91 <leblancsm91@gmail.com>
2019-07-26 13:44:59 -0700

    [new drill]

---
+++ c# course/class_method_drill2/class_method_drill2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_method_drill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            math solution = new math();
+
+            solution = Add(math: solution);
+        }
+    }
+}
---
+++ c# course/class_method_drill2/class_method_drill2/math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_method_drill2
+{
+    public class math
+    {
+        public void Add(int numbers)
+        {
+            int numb1 = 1 + 2;
+            int numb2 = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
