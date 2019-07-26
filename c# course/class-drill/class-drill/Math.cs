8d6f54911c325d9b036440aa8106146c44237f05
leblancsm91 <leblancsm91@gmail.com>
2019-07-26 11:15:28 -0700

    [added new drill]

---
+++ c# course/class-drill/class-drill/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_drill
+{
+    public class Math
+    {
+        public Math()
+        {
+        }
+        public int Add(int number1)
+        {
+            return number1 + 50;
+        }
+        public int Subt(int number2)
+        {
+            return number2 - 10;
+        }
+
+        public int Mult(int number3)
+        {
+            return number3 * 5;
+        }
+    }
+}
---
+++ c# course/class-drill/class-drill/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math solution = new Math();
+            
+            Console.WriteLine("type a number");
+            int number1 = Convert.ToInt32(Console.ReadLine()); //storing variable one here // always has to convert like this
+            int result1 = solution.Add(number1); //storing results to variable result1 
+            Console.WriteLine(result1);
+            Console.ReadLine();
+
+            Console.WriteLine("type another number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int result2 = solution.Subt(number2);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+            Console.WriteLine("type a numbe to be multiplied");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            int result3 = solution.Mult(number3);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+        }
+    }
+}
