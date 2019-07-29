da2e621e1d559ea3a3a687835398980f5c4ce003
leblancsm91 <leblancsm91@gmail.com>
2019-07-29 11:43:05 -0700

    [new drill]

---
+++ c# course/method-Drill5/method-Drill5/division.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_Drill5
+{
+    public class division
+    {
+        public static void Divide(out int numb1)
+        {
+            return numb1 / 2;
+        }
+
+        public static Divide(int numb1, numb2)
+        {
+            return numb1 + numb2 / 2;
+        }
+    }
+}
