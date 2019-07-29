89f84ca18f100554d5ec95e2953caa6d41d96782
leblancsm91 <leblancsm91@gmail.com>
2019-07-29 10:16:34 -0700

    [added new drill]

---
+++ c# course/class-methodDrill4/class-methodDrill4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_methodDrill4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            math solution = new math();
+
+            Console.WriteLine("enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int result = solution.Add(a, b);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            int b = Convert.ToInt32(Console.ReadLine());
+            int result2 = solution.Add(a, int b = 2);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ c# course/class-methodDrill4/class-methodDrill4/math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_methodDrill4
+{
+    public class math
+    {
+        public int Add(int a, int b = 2)
+        {
+            return a + 1, b + 2;
+        }
+        
+
+    }
+}
