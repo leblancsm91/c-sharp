d5c711d7f60267c667fd9e7df92780e3b26c0245
leblancsm91 <leblancsm91@gmail.com>
2019-07-30 09:38:10 -0700

    [change drill]

--- c# course/carins_drill/carins_drill/MainWindow.xaml.cs
+++ c# course/carins_drill/carins_drill/MainWindow.xaml.cs
@@ -8,16 +8,26 @@ class Program
     static void Main()
         {
         Console.WriteLine("What is your age?");
-        int age = Console.ReadLine(if > 15 = true, else = false);
+        int age = Convert.ToInt32(Console.ReadLine(if > 15));
 
         Console.WriteLine("Have you ever had a DUI?");
-        bool dui = Console.ReadLine(if no = true, else = false);
+        string dui = Console.ReadLine(if no = true, else = false);
 
         Console.WriteLine("How many sppeeding tickets do you have?");
-        int tickets = Console.ReadLine(if < 3 = true, else = false);
+        int tickets = Convert.ToInt32(Console.ReadLine(if < 3 = tr));
 
-        Console.WriteLine("you are " + answer + "qualified for car insurance with this fake insurance company.");
-        bool answer = Console.ReadLine(if true = yes, else if false = no);
+        string approval = "";
+        if (age > 15 && dui == "no" && tickets < 3)
+        {
+            approval = "are";
+        }
+        else
+        {
+            approval = "are not";
+        }
+
+        Console.WriteLine("you " + approval + "qualified for car insurance with this fake insurance company.");
+       
     }
 
 }
