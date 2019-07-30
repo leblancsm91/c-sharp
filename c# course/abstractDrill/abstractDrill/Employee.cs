6bee52a5def04859022961797dd93bdfbc434569
leblancsm91 <leblancsm91@gmail.com>
2019-07-30 11:45:39 -0700

    [new drill]

---
+++ c# course/abstractDrill/abstractDrill/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractDrill
+{
+    public class Employee : Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("");
+        }
+
+
+    }
+}
---
+++ c# course/abstractDrill/abstractDrill/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractDrill
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void SayName()
+        {
+
+        }
+
+    }
+}
