e527b2ef6af82192a4d3bcbbb91ef7aed37f05e6
leblancsm91 <leblancsm91@gmail.com>
2019-07-31 12:12:55 -0700

    [new drill]

---
+++ c# course/enumDrill/enumDrill/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfWeek day = new DaysOfWeek();
+            Console.WriteLine("what is the day today?");
+
+            try
+            {
+                string answer = (Console.ReadLine().ToUpper());
+            }
+            catch ()
+            {
+                Console.WriteLine("")
+            }
+
+        }
+        public enum DaysOfWeek
+        {
+            Monday,
+            Tuesday, 
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+}
