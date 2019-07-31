a52a1a6f1f762f1f7eef64a9b22f719ddf42aa45
leblancsm91 <leblancsm91@gmail.com>
2019-07-31 13:21:24 -0700

    [new drill]

---
+++ c# course/structDrill/structDrill/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structDrill
+{
+    public struct Number
+    {
+        public Number Amount { get; set; }
+    }
+    public enum Amount<float>();
+}
---
+++ c# course/structDrill/structDrill/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number Amount = new Number();
+            float dec = 1.5;
+        }
+    }
+}
