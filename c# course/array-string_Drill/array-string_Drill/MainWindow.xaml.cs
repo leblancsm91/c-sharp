2d13a96d46608323a66c35401f9b5c6d8b4a3265
leblancsm91 <leblancsm91@gmail.com>
2019-08-01 10:33:10 -0700

    [edit to code]

--- c# course/array-string_Drill/array-string_Drill/MainWindow.xaml.cs
+++ c# course/array-string_Drill/array-string_Drill/MainWindow.xaml.cs
@@ -3,35 +3,54 @@ using System.Collections.Generic;
 
 class Program
 {
-    static void Main();
+    public static void Main()
+    {
 
-    string[] colorArray = new string[] { red, orange, yellow, green, blue, purple };
+        string[] colorArray = new string[] { "red", "orange", "yellow", "green", "blue", "purple" };
+
+        List<string> intList = new List<string>();
+        stringList.Add(red);
+        stringList.Add(orange);
+        stringList.Add(yellow);
+        stringList.Add(green);
+        stringList.Add(blue);
+        stringList.Add(purple);
+
+        Console.WriteLine("choose a color from these : ");
+        for (int i = 0; i < colorArray.Length; i += 1)
+        {
+            Console.WriteLine("{0} = {1}", i, colorArray[i]);
+        }
 
 
-    Console.WriteLine("please pick a box from this array" + colorArray[] + );
-    Console.ReadLine(if 0, 1, 2, 3, 4, 5 = colorArray[0], colorArray[1], colorArray[2], colorArray[3], colorArray[4], colorArray[5], else = "this is not a real box of array");
 
-    in[] numArray = new int[6];
-    numArray[0] = 10;
-    numArray[1] = 100;
-    numArray[2] = 20;
-    numArray[3] = 200;
-    numArray[4] = 30;
-    numArray[5] = 300;
+        int[] numArray = new int[6];
+        numArray[0] = 10;
+        numArray[1] = 100;
+        numArray[2] = 20;
+        numArray[3] = 200;
+        numArray[4] = 30;
+        numArray[5] = 300;
 
-    Console.WriteLine("please choose a box from this array" + numArray[] + );
-    Console.ReadLine(if 0, 1, 2, 3, 4, 5 = numArray[0], numArray[1], numArray[2], numArray[3], numArray[4], numArray[5], else = "that is not an array box");
-    
-        //list
-    List<string> intList = new List<string>();
-    stringList.Add(red);
-    stringList.Add(orange);
-    stringList.Add(yellow);
-    stringList.Add(green);
-    stringList.Add(blue);
-    stringList.Add(purple);
+        bool start = true;
+        while (start)
+        {
+            Console.WriteLine("choose number from: ");
+            int numb = Convert.ToInt32(Console.ReadLine());
+        }
 
-    Console.WriteLine(intList[0]); //<-always assume have array!
+        if (numb < colorArray.Length)
+        {
+            Console.WriteLine("you've chosen '{0}'!", colorArray[numb]);
+            start = false;
+        }
+        else
+        {
+            Console.WriteLine("nope! try another choice");
+
+        }
     Console.ReadLine();
+    }
 
+    
 }
