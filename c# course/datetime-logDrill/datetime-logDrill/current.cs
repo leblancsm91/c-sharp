e6ed02878ca9c7ee170817280141cbedc55b7a13
leblancsm91 <leblancsm91@gmail.com>
2019-08-02 12:18:12 -0700

    [new drill]

---
+++ c# course/datetime-logDrill/datetime-logDrill/current.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datetime_logDrill
+{
+    public class current
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+        }
+    }
+}
