48ddd37b81dd142d8851c010926881aa98375c5f
leblancsm91 <leblancsm91@gmail.com>
2019-08-02 16:00:30 -0700

    [new drill]

---
+++ c# course/var-constDrill/var-constDrill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var_constDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var mySpace = new Old();
+
+            const string youAre = "you are";
+
+            Console.WriteLine("do you like myspace,  or use myspce?");
+            string answer = Console.ReadLine();
+
+            if (answer == "yes" || answer == "yeah")
+            {
+                Console.WriteLine(youAre + " old");
+
+            }
+            else
+            {
+                Console.WriteLine(youAre + " not as old");
+            }
+            Console.ReadLine();
+        }
+    }
+}
