c474b4a679f2c5f559ac383edb2db1b3e64b3663
leblancsm91 <leblancsm91@gmail.com>
2019-08-02 10:53:31 -0700

    [new drill]

---
+++ c# course/log-txtDrill/log-txtDrill/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log_txtDrill
+{
+    public sclass Class1
+    {
+        string randomClass = string.Format(class2.randomClass.First());
+        Console.WriteLine(randomClass);
+        using (StreamWriter File = new StreamWriter(@"C:\\Users\\Owner\\c-sharp\\c# course\\log1.txt", true))
+        {
+            File.WriteLine(randomClass);
+        }
+    }
+}
---
+++ c# course/log-txtDrill/log-txtDrill/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log_txtDrill
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string File.ReadAllText(@"C:\\Users\\Documents\\GitHub\\c-sharp\\c# course\\log1.txt");
+
+        }
+    }
+}
