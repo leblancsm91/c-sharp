190c2348d6177e27a2978a43bb217e281b7ac1aa
leblancsm91 <leblancsm91@gmail.com>
2019-08-22 11:03:19 -0700

    [made changes]

---
+++ c# course/bool_drillDOWHILE-94/bool_comparisondrillDOWHILE/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boolean_comparisondrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color = "";
+            bool go = true;
+
+            do
+            {
+                Console.WriteLine("choose a color: \nRed \nOrange \nYellow \nGreen \nBlue \nPurple");
+                color = Console.ReadLine().ToUpper();
+                if (color == "RED" || color == "ORANGE")
+                {
+                    Console.WriteLine("{0}} was the right color!", color);
+                    go = false;
+                }
+
+                else
+                {
+                    Console.WriteLine(" {0} was an incorrect color", color);
+                    break;
+                }
+            }
+            while (go == true);
+            Console.WriteLine("Good job!");
+            Console.ReadLine();
+        }
+    }
+}
