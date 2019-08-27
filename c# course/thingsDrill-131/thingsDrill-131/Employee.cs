d813475ae03d5d99c674bff0eec918154c102b64
leblancsm91 <leblancsm91@gmail.com>
2019-08-27 16:08:28 -0700

    [rewrote code]

---
+++ c# course/thingsDrill-131/thingsDrill-131/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thingsDrill_131
+{
+    public class Employee<T>
+    {
+        public List<T> things;
+
+    }
+}
---
+++ c# course/thingsDrill-131/thingsDrill-131/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thingsDrill_131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> thang = new Employee<string>();
+            thang.things = new List<string>() { "these", "are", "things" };
+
+            Employee<int> IDs = new Employee<int>();
+            IDs.things = new List<int>() { 001, 002, 003 };
+
+
+
+            foreach (thang in things)
+            {
+                Console.ReadLine(thang);
+            }
+
+
+
+
+
+
+            for (int i = 0; i < thang.things.Count; i++)
+            {
+                Console.WriteLine(IDs.things[i] + " " + thang.things[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
